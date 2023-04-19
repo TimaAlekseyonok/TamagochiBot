d8faf9329f767008b781b911c6a9d0c0a7e1d3ad
unknown <nbv.alekss@mail.ru>
2023-04-19 17:48:05 +0300

    v.2.0

--- life.py
+++ life.py
@@ -1,157 +1,75 @@
-import schedule
 import time
 import threading
-
+from dict_id_chats import user_dict
 import telebot
 from telebot import types
-from chat_id import ChatIDSingleton
-from health import Health
-
 
 bot = telebot.TeleBot('6202651990:AAEsQjIPBDqsMOx5wMjdFYEH44kMumWFMsU')
-id_chata = None
 
+from pet import Pet
 
-pet_health = None
-last_health = 10
-start_life = False
-need_food = False
-need_walk = False
-need_wash = False
-
-last_time_food = None
-last_time_walk = None
-last_time_wash = None
-
-
-def id_life():
-    global id_chata
-    id_chata = ChatIDSingleton()
-
-
-
-def get_damage():
-    global pet_health
-    pet_health.damage()
-    if pet_health.health > 0:
-        bot.send_message(id_chata.chat_id, f'Здоровье вашего питомца уменьшилось на 1, теперь его показатель здоровья составляет {pet_health.health} из 10!')
-    else:
-        bot.send_message(id_chata.chat_id, 'Ваш питомец погиб! Он был с вами так мало времени, но он останется в вашем сердце навсегда.')
-        global start_life
-        start_life = False
-
-def check_heel():
-    global last_health, pet_health
-    if pet_health.health < 10:
-        if last_health > pet_health.health:
-            last_health = pet_health.health
-        elif last_health == pet_health.health:
-            pet_health.heel()
-            last_health = pet_health.health
-            bot.send_message(id_chata.chat_id, '+1')
-
-
-
-def check_food():
-    global need_food
-    if need_food is False:
-        bot.send_message(id_chata.chat_id, "Спасибо, что покормили питомца!")
-    else:
-        bot.send_message(id_chata.chat_id, "Вы не успели покормить питомца, он теряет 1 здоровье :(")
-        need_food = False
-        get_damage()
-
-def whaiting_food():
-    global last_time_food
-    while time.time() - last_time_food < 30*60 and need_food is True:
-        time.sleep(1)
-    else:
-        check_food()
-
-def create_potok_food(whaiting_food_func):
-    potok_food = threading.Thread(target=whaiting_food_func)
-    potok_food.start()
 
 def feed_pet():
-    global need_food
-    bot.send_message(id_chata.chat_id, "Ваш питомец проголодался. Его нужно покормить.")
-    need_food = True
-    global last_time_food
-    last_time_food = time.time()
-    create_potok_food(whaiting_food)
+    def feed_potok(key, pet):
+        pet.food()
+        last_time_food = time.time()
+        while time.time() - last_time_food < 60*30:
+            time.sleep(1)
+        if pet.need_food['need_food'] and pet.life_status['life_status'] == True:
+            bot.send_message(key, "Вы не успели покормить питомца, он теряет 1 здоровье :(")
+            pet.got_food()
+            pet.damage()
+
+    for key, pet in user_dict.items():
+        pet.food()
+        potok = threading.Thread(target=feed_potok, args=(key, pet,))
+        potok.start()
 
 
 
-def check_walk():
-    global need_walk
-    if need_walk is False:
-        bot.send_message(id_chata.chat_id, "Ура питомец погулял!")
-    else:
-        bot.send_message(id_chata.chat_id, "Вы не погуляли с малышом :(")
-        need_walk = False
-        get_damage()
+def walk_pet():
+    def walk_potok(key, pet):
+        pet.walk()
+        last_time_walk = time.time()
+        while time.time() - last_time_walk < 60*30:
+            time.sleep(1)
+        if pet.need_walk['need_walk'] and pet.life_status['life_status'] == True:
+            bot.send_message(key, "Вы не успели погулять с питомцем, он теряет 1 здоровье :(")
+            pet.got_walk()
+            pet.damage()
 
-def whaiting_walk():
-    global last_time_walk
-    while time.time() - last_time_walk < 30*60 and need_walk is True:
-        time.sleep(1)
-    else:
-        check_walk()
+    for key, pet in user_dict.items():
+        pet.walk()
+        potok = threading.Thread(target=walk_potok, args=(key, pet,))
+        potok.start()
 
-def create_potok_walk(whaiting_walk_func):
-    potok_walk = threading.Thread(target=whaiting_walk_func)
-    potok_walk.start()
-
-def take_pet_out():
-    global need_walk
-    bot.send_message(id_chata.chat_id, "Ваш питомец хочет на улицу.")
-    need_walk = True
-    global last_time_walk
-    last_time_walk = time.time()
-    create_potok_walk(whaiting_walk)
-
-
-
-def check_wash():
-    global need_wash
-    if need_wash is False:
-        bot.send_message(id_chata.chat_id, "Ура, мы чистюли!")
-    else:
-        bot.send_message(id_chata.chat_id, "Мы свинтусы :(")
-        need_wash = False
-        get_damage()
-
-def whaiting_wash():
-    global last_time_wash
-    while time.time() - last_time_wash < 30*60 and need_wash is True:
-        time.sleep(1)
-    else:
-        check_wash()
-
-def create_potok_wash(whaiting_wash_func):
-    potok_wash = threading.Thread(target=whaiting_wash_func)
-    potok_wash.start()
 
 def wash_pet():
-    global need_wash
-    bot.send_message(id_chata.chat_id, "Ваш питомец вымазался, его нужно помыть.")
-    need_wash = True
-    global last_time_wash
-    last_time_wash = time.time()
-    create_potok_wash(whaiting_wash)
+    def wash_potok(key, pet):
+        pet.wash()
+        last_time_wash = time.time()
+        while time.time() - last_time_wash < 60*30:
+            time.sleep(1)
+        if pet.need_wash['need_wash'] and pet.life_status['life_status'] == True:
+            bot.send_message(key, "Вы не успели помыть питомца, он теряет 1 здоровье :(")
+            pet.got_wash()
+            pet.damage()
+
+    for key, pet in user_dict.items():
+        pet.wash()
+        potok = threading.Thread(target=wash_potok, args=(key, pet,))
+        potok.start()
+
+
+def check_heel():
+    for key, pet in user_dict.items():
+        if pet.hp < 10:
+            if pet.last_hp > pet.hp:
+                pet.last_hp = pet.hp
+            elif pet.last_hp == pet.hp:
+                pet.heel()
+                pet.last_hp = pet.hp
+                bot.send_message(key, 'Вы хорошо следили за питомцем, он чувствует себя замечательно и получает 1 здоровье')
 
 
 
-schedule.every(61).minutes.do(feed_pet)
-schedule.every(123).minutes.do(take_pet_out)
-schedule.every(298).minutes.do(wash_pet)
-schedule.every().day.at('03:00').do(check_heel)
-
-
-
-def start_life_funktion():
-    global pet_health
-    pet_health = Health(10)
-    while start_life is True:
-        schedule.run_pending()
-        time.sleep(1)
--- main.py
+++ main.py
@@ -1,92 +1,223 @@
+import schedule
+import time
+import datetime
 import telebot
+import threading
 from telebot import types
-from chat_id import ChatIDSingleton
-import pets
-import prove_pet
-import life
+from dict_id_chats import user_dict
+from info_pets import info_dog, info_cat, info_panda, food_meet, food_green, food_all
+from pet import Pet
+from life import feed_pet, walk_pet, wash_pet, check_heel
 
 
 bot = telebot.TeleBot('6202651990:AAEsQjIPBDqsMOx5wMjdFYEH44kMumWFMsU')
 
 
-id_chata = None
-
-
 @bot.message_handler(commands=['start'])
 def hello(message):
-    global id_chata
-    id_chata = ChatIDSingleton()
-    id_chata.set_chat_id(message.chat.id)
-
     tip_pitomca = types.InlineKeyboardMarkup()
     dog = types.InlineKeyboardButton('🐶', callback_data='dog')
     cat = types.InlineKeyboardButton('🐱', callback_data='cat')
     panda = types.InlineKeyboardButton('🐼', callback_data='panda')
     tip_pitomca.add(dog, cat, panda)
-    bot.send_message(id_chata.chat_id, 'Привет, давай выберем твоего питомца!', reply_markup=tip_pitomca)
+    bot.send_message(message.chat.id, 'Привет, давай выберем твоего питомца!', reply_markup=tip_pitomca)
 
-    pets.id_pet()
-    prove_pet.id_prove()
-    life.id_life()
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'dog')
+def dog(call):
+    prove = types.InlineKeyboardMarkup()
+    button_prove = types.InlineKeyboardButton('Подтвердить выбор', callback_data='prove_dog')
+    button_exit = types.InlineKeyboardButton('Отмена', callback_data='exit')
+    prove.add(button_prove, button_exit)
+    bot.send_message(call.from_user.id, '🐶 Гав-Гав')
+    bot.send_message(call.from_user.id, f'{info_dog}', reply_markup=prove)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'cat')
+def cat(call):
+    prove = types.InlineKeyboardMarkup()
+    button_prove = types.InlineKeyboardButton('Подтвердить выбор', callback_data='prove_cat')
+    button_exit = types.InlineKeyboardButton('Отмена', callback_data='exit')
+    prove.add(button_prove, button_exit)
+    bot.send_message(call.from_user.id, '🐱 Мяу Мяу')
+    bot.send_message(call.from_user.id, f'{info_cat}', reply_markup=prove)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'panda')
+def panda(call):
+    prove = types.InlineKeyboardMarkup()
+    button_prove = types.InlineKeyboardButton('Подтвердить выбор', callback_data='prove_panda')
+    button_exit = types.InlineKeyboardButton('Отмена', callback_data='exit')
+    prove.add(button_prove, button_exit)
+    bot.send_message(call.from_user.id, '🐼 Нгяньгь')
+    bot.send_message(call.from_user.id, f'{info_panda}', reply_markup=prove)
+
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'exit')
+def exit(call):
+    bot.delete_message(call.from_user.id, call.message.message_id)
+    bot.delete_message(call.from_user.id, call.message.message_id - 1)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'prove_dog')
+def prove_dog(call):
+    bot.send_message(call.from_user.id, 'Отлично теперь 🐶 ваш питомец! Придумайте ему имя и напишите его!')
+    bot.register_next_step_handler(call.message, named_pet)
+    global pet_tipe
+    pet_tipe = '🐶'
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'prove_cat')
+def prove_cat(call):
+    bot.send_message(call.from_user.id, 'Отлично теперь 🐱 ваш питомец! Придумайте ему имя и напишите его!')
+    bot.register_next_step_handler(call.message, named_pet)
+    global pet_tipe
+    pet_tipe = '🐱'
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'prove_panda')
+def prove_panda(call):
+    bot.send_message(call.from_user.id, 'Отлично теперь 🐼 ваш питомец! Придумайте ему имя и напишите его!')
+    bot.register_next_step_handler(call.message, named_pet)
+    global pet_tipe
+    pet_tipe = '🐼'
+
+
+def named_pet(message):
+    pet_name = message.text
+    bot.send_message(message.chat.id, f'Отлично, {pet_name} - хорошее имя для {pet_tipe}!')
+    moy_pet = Pet(message.chat.id, pet_tipe, True, 10, 10, pet_name, False, False, False, time.time())
+    user_dict[message.chat.id] = moy_pet
+    print(user_dict)
+    if len(user_dict) == 1:
+        start_check()
+
+
+
+def check():
+    def check_potok(key, pet):
+        if pet.life_status['life_status'] == True and pet.life_status['message'] == False:
+            if pet.need_food['need_food'] == True and pet.need_food['message'] == False:
+                bot.send_message(key, f'Питомец голоден!')
+                pet.need_food['message'] = True
+            if pet.need_walk['need_walk'] == True and pet.need_walk['message'] == False:
+                bot.send_message(key, f'Питомец хочет на улицу!')
+                pet.need_walk['message'] = True
+            if pet.need_wash['need_wash'] == True and pet.need_wash['message'] == False:
+                bot.send_message(key, f'Питомца пора помыть!')
+                pet.need_wash['message'] = True
+        elif pet.life_status['life_status'] == False and pet.life_status['message'] == False:
+            time_diff = datetime.timedelta(seconds=time.time() - pet.time)
+            days, seconds = time_diff.days, time_diff.seconds
+            hours = seconds // 3600
+            minutes = (seconds % 3600) // 60
+            seconds = seconds % 60
+
+            message_text = f"{days} дней, {hours} часов, {minutes} минут, {seconds} секунд"
+            bot.send_message(key, f'Ваш питомец погиб! Он был с вами так мало времени, но он останется в вашем сердце навсегда.\n'
+                                  f'Ваш питомец был с вами:\n'
+                                  f'{message_text}')
+            pet.life_status['message'] = True
+
+    for key, pet in user_dict.items():
+        print(key)
+        potok = threading.Thread(target=check_potok, args=(key, pet,))
+        potok.start()
+
+
+schedule.every(5).minutes.do(check)
+schedule.every(61).minutes.do(feed_pet)
+schedule.every(92).minutes.do(walk_pet)
+schedule.every(119).minutes.do(wash_pet)
+schedule.every().day.at('10:00').do(check_heel)
+
+
+def start_check():
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
+
 
 
 @bot.message_handler(commands=['help'])
 def info(message):
-    bot.send_message(id_chata.chat_id, f'Вот список всех команд которые доступны в этом боте: \n'
-                                      f'1. ... \n'
-                                      f'2. ... \n'
-                                      f'3. ...')
+    bot.send_message(message.chat.id, f'Вот список всех команд которые доступны в этом боте: \n'
+                                      f'1. /start - Начинает создание вашего питомца \n'
+                                      f'2. /info - Выводит на экран информацию о вашем питомце')
 
 
-@bot.message_handler(commands=['name'])
+@bot.message_handler(commands=['keel'])
 def info(message):
-    if prove_pet.pitomec_name is None:
+    if message.chat.id not in user_dict:
         bot.send_message(message.chat.id, 'Вы пока ещё не завели питомца( \n'
                                           'Давайте это исправим, жмите /start !')
     else:
-        bot.send_message(message.chat.id, f'Ваш питомец - {prove_pet.pitomec_name}')
+        bot.send_message(message.chat.id, f'Вы бесчеловечно убили вашего питомца. Чем же {user_dict[message.chat.id].name} заслужил(ла) такого обращения? У вас нет ничего святого. Удалите, пожалуйста, этого бота и никогда сюда не возвращайтесь')
+        user_dict[message.chat.id].hp = 0
+        user_dict[message.chat.id].life_status['life_status'] = False
+        check()
 
-@bot.message_handler(commands=['health'])
+
+@bot.message_handler(commands=['info'])
 def info(message):
-    if life.pet_health is None:
+    if message.chat.id not in user_dict:
         bot.send_message(message.chat.id, 'Вы пока ещё не завели питомца( \n'
                                           'Давайте это исправим, жмите /start !')
     else:
-        bot.send_message(message.chat.id, f'Здоровье вашего питомца - {life.pet_health.health} из 10!')
+        time_diff = datetime.timedelta(seconds=time.time() - user_dict[message.chat.id].time)
+        days, seconds = time_diff.days, time_diff.seconds
+        hours = seconds // 3600
+        minutes = (seconds % 3600) // 60
+        seconds = seconds % 60
 
+        message_text = f"{days} дней, {hours} часов, {minutes} минут, {seconds} секунд"
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    if call.message:
-
-        if call.data == 'dog':
-            pets.dog(call)
-        elif call.data == 'cat':
-            pets.cat(call)
-        elif call.data == 'panda':
-            pets.panda(call)
-        elif call.data == 'exit':
-            pets.exit(call)
-
-        else:
-            pets.callback_inline(call)
-
-
-@bot.message_handler(func=lambda message: prove_pet.pet_chosen == True)
-def message(message):
-    prove_pet.message(message)
+        bot.send_message(message.chat.id, f'Ваш питомец - {user_dict[message.chat.id].tipe}\n'
+                                          f'Имя вашего питомца - {user_dict[message.chat.id].name}\n'
+                                          f'Здоровье вашего питомца - {user_dict[message.chat.id].hp} из 10!\n'
+                                          f'Ваш питомец с вами уже:\n'
+                                          f'{message_text}')
 
 
 @bot.message_handler()
 def message(message):
+    print(message)
     if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, 'прив')
-    elif message.text == '🍗':
-        life.need_food = False
-    elif message.text == 'гуляю':
-        life.need_walk = False
-    elif message.text == 'мою':
-        life.need_wash = False
+        print('есть контакт')
+    if message.text.lower() == 'гуляю':
+        user_dict[message.chat.id].got_walk()
+        bot.send_message(message.chat.id, 'питомец погулял')
+    if message.text.lower() == 'мою':
+        user_dict[message.chat.id].got_wash()
+        bot.send_message(message.chat.id, 'питомец помыт')
+
+    for i in message.text:
+        if i in food_all:
+            if user_dict[message.chat.id].tipe == '🐶':
+                if i in food_meet:
+                    user_dict[message.chat.id].got_food()
+                    bot.send_message(message.chat.id, f'Питомец покормлен! {user_dict[message.chat.id].name} в восторге от {i}')
+                else:
+                    bot.send_message(message.chat.id, f'{i} - эта еда {user_dict[message.chat.id].name} не понравилась, не давайте больше такое вашему питомцу.')
+
+            if user_dict[message.chat.id].tipe == '🐱':
+                if i in food_meet:
+                    user_dict[message.chat.id].got_food()
+                    bot.send_message(message.chat.id, f'Питомец покормлен! {user_dict[message.chat.id].name} в восторге от {i}')
+                else:
+                    bot.send_message(message.chat.id, f'{i} - эта еда {user_dict[message.chat.id].name} не понравилась, не давайте больше такое вашему питомцу.')
+
+            if user_dict[message.chat.id].tipe == '🐼':
+                if i in food_green:
+                    user_dict[message.chat.id].got_food()
+                    bot.send_message(message.chat.id,
+                                     f'Питомец покормлен! {user_dict[message.chat.id].name} в восторге от {i}')
+                else:
+                    bot.send_message(message.chat.id,
+                                     f'{i} - эта еда {user_dict[message.chat.id].name} не понравилась, не давайте больше такое вашему питомцу.')
 
 
 
